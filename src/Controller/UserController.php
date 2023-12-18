a6e14dff57c598150cb8124c176befc5afa8383e
Somenae <kevindevaux@neuf.fr>
2023-12-18 15:18:54 +0100

    hash password done

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -9,6 +9,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 #[Route(path:'user/')]
@@ -18,13 +19,20 @@ class UserController extends AbstractController
     public function new(
         Request $request,
         EntityManagerInterface $em,
+        UserPasswordHasherInterface $hasher,
     ): Response
     {
         $user = new Users();
         $form = $this->createForm(UserFormType::class, $user);
-
+        $plainTextPassword = '';
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
+            $plainTextPassword = $_POST['user_form']['password']['first'];
+            $hashedPassword = $hasher->hashPassword(
+                $user,
+                $plainTextPassword
+            );
+            $user->setPassword($hashedPassword);
             $em->persist($user);
             $em->flush();
             return $this->redirectToRoute('app_login');
