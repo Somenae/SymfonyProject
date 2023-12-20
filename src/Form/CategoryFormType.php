0c9efddaba6ea2b060437132e2e4713d40432f06
Thiblt <t.pouplard@gmx.fr>
2023-12-20 10:16:39 +0100

    Bug fix

--- src/Form/CategoryFormType.php
+++ src/Form/CategoryFormType.php
@@ -9,7 +9,7 @@ use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class categoryFormType extends AbstractType
+class CategoryFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
