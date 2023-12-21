9c69159eb2d4b331a9261902c8c9222daaa7a353
Thiblt <t.pouplard@gmx.fr>
2023-12-21 15:16:43 +0100

    Cart gestion incoming

---
+++ src/Controller/CartController.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Cart;
+use App\Entity\CartLine;
+use App\Entity\Product;
+use App\Repository\CartRepository;
+use App\Repository\ProductRepository;
+use DateTime;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\SecurityBundle\Security;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+class CartController extends AbstractController
+{
+
+    public function cartCount(CartRepository $cartrepo, Security $security ): Response
+    {
+
+        $cart=NULL;
+        if( $security->isGranted('ROLE_USER')){
+            $cart = $cartrepo->findLastCartByIdUser($security->getUser()->getId());
+        }
+        return $this->render('_header/_panier.html.twig', [
+            'cart' =>  $cart,
+        ]);
+}
+     #[Route('/cartDisplay', name: 'app_cart_display')]
+     public function cartDisplay(CartRepository $cartrepo, Security $security ): Response
+     { if( !($security->isGranted('ROLE_USER'))){
+        return $this->redirectToRoute('app_login');
+     }
+
+        $cart=NULL;
+        $price=0 ;
+        if($cart !== NULL){
+            $cart = $cartrepo->findLastCartByIdUser($security->getUser()->getId());
+            $cartline =  $cart->getCartLine();
+
+            foreach($cartline as $line){
+                $pricel= + ($line->getQuantity())*($line->getProduct()->getPrice());
+               
+                if ($line->getProduct()->getProductTaxes() !== NULL){
+                    $pricel = $pricel *(1-(($line->getProduct()->getProductTaxes()->getAmount())/100));
+
+                }
+                if ($line->getProduct()->getProductSales() !== NULL){
+                    $pricel = $pricel *(1-(($line->getProduct()->getProductSales()->getAmountPercentage())/100));
+                    echo($line->getProduct()->getProductSales()->getAmountPercentage()%100);
+                    echo " Poucentage de reduc    " ;
+                }
+                $price = $price +  $pricel;
+
+                return $this->render('pages/cartDisplay.html.twig', [
+                    'cartline' =>  $cartline,
+                    'price' => $price
+                ]);
+
+            }
+        }
+        return $this->render('pages/cartDisplay.html.twig', [
+            'cartline' =>  NULL ,
+        ]);
+       
+    }
+
+    // NON TESTE
+    #[Route('/AddtoCart{id}', name: 'app_cart_add')]
+    public function AddtoCart(CartRepository $cartrepo, Security $security, Product $product, EntityManagerInterface $em ) : Response
+    { if( !($security->isGranted('ROLE_USER'))){
+       return $this->redirectToRoute('app_login');
+    }
+
+ 
+           $cart = $cartrepo->findLastCartByIdUser($security->getUser()->getId());
+           if ($cart === NULL)
+           {
+            $cart = new Cart;
+            // $cart->setCreationDate(getDate()["year"]."-".getDate()["mon"]."-".getDate()["mday"]);
+            $cart->setCreationDate(new DateTime());
+           }
+           $cartline = new CartLine;
+           $cartline->setQuantity($_POST('quantity'));
+           $cartline->setProduct($product);
+           $cartline->setProduct($product);
+           $cartline->setCart($cart);
+           $em->persist($cart);
+           $em->persist($cartline);
+           $em->flush();
+           return $this->redirectToRoute('app_cart_display');
+    }
+
+    #[Route('/RemoveFromCart{id}', name: 'app_cart_remove')]
+    public function RemoveFromCart(CartRepository $cartrepo, Security $security, Cartline $cartline, EntityManagerInterface $em ) : Response
+    { if( !($security->isGranted('ROLE_USER'))){
+       return $this->redirectToRoute('app_login');
+    }
+           $em->remove($cartline);
+           $em->flush();
+           return $this->redirectToRoute('app_cart_display');
+    }
+   
+}
--- src/Controller/PagesController.php
+++ src/Controller/PagesController.php
@@ -31,49 +31,5 @@ class PagesController extends AbstractController
         ]);
     }
     
-    public function cartCount(CartRepository $cartrepo, Security $security ): Response
-    {
-
-        $cart=NULL;
-        if( $security->isGranted('ROLE_USER')){
-            $cart = $cartrepo->findLastCartByIdUser($security->getUser()->getId());
-        }
-        return $this->render('_header/_panier.html.twig', [
-            'cart' =>  $cart,
-        ]);
-}
-     #[Route('/cartDisplay', name: 'app_cart_display')]
-     public function cartDisplay(CartRepository $cartrepo, Security $security ): Response
-     { if( !($security->isGranted('ROLE_USER'))){
-        return $this->redirectToRoute('app_login');
-     }
-
-        $cart=NULL;
-        $price=0 ;
-        if( $security->isGranted('ROLE_USER')){
-            $cart = $cartrepo->findLastCartByIdUser($security->getUser()->getId());
-            $cartline =  $cart->getCartLine();
-
-            foreach($cartline as $line){
-                $pricel= + ($line->getQuantity())*($line->getProduct()->getPrice());
-               
-                if ($line->getProduct()->getProductTaxes() !== NULL){
-                    $pricel = $pricel *(1-(($line->getProduct()->getProductTaxes()->getAmount())/100));
-
-                }
-                if ($line->getProduct()->getProductSales() !== NULL){
-                    $pricel = $pricel *(1-(($line->getProduct()->getProductSales()->getAmountPercentage())/100));
-                    echo($line->getProduct()->getProductSales()->getAmountPercentage()%100);
-                    echo " Poucentage de reduc    " ;
-                }
-                $price = $price +  $pricel;
-
-
-            }
-        }
-        return $this->render('pages/cartDisplay.html.twig', [
-            'cartline' =>  $cartline,
-            'price' => $price
-        ]);
-    }
+    
 }
